033d5a88bdeaca136980a1373cd59a16e4de6c8e
Jiaying <jiayingyong8@gmail.com>
2024-05-03 18:34:13 +0800

    Fix: minor merge error

--- app/src/main/java/com/example/apapunada/ApaPunAda.kt
+++ app/src/main/java/com/example/apapunada/ApaPunAda.kt
@@ -41,10 +41,10 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navigation
 import com.example.apapunada.ui.IntroductionPager
 import com.example.apapunada.ui.LoginScreen
+import com.example.apapunada.ui.MenuScreen
 import com.example.apapunada.ui.components.StaffAppBarPortrait
 import com.example.apapunada.ui.staff.StaffDashboardScreen
 import com.example.apapunada.ui.users.HomeScreen
-import com.example.apapunada.ui.users.MenuScreen
 import com.example.apapunada.ui.users.MoreScreen
 import com.example.apapunada.ui.users.OrderOptionScreen
 import com.example.apapunada.ui.users.RewardsScreen
--- app/src/main/java/com/example/apapunada/ui/users/FoodDetailScreen.kt
+++ app/src/main/java/com/example/apapunada/ui/users/FoodDetailScreen.kt
@@ -1,9 +1,7 @@
-package com.example.apapunada.ui
+package com.example.apapunada.ui.users
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,20 +17,14 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -45,12 +37,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.apapunada.R
-import com.example.apapunada.data.MenuSample
 import com.example.apapunada.data.MenuSample.Menus
 import com.example.apapunada.model.Menu
 import com.example.apapunada.ui.components.MyTopTitleBar
