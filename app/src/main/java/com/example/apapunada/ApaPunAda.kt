f6735fde39f69eef55c2611d9a23651b21108ad5
Kong Weng Cheng <chipndale113.dc@gmail.com>
2024-05-06 09:47:38 +0800

    Add: Waitlist screen

--- app/src/main/java/com/example/apapunada/ApaPunAda.kt
+++ app/src/main/java/com/example/apapunada/ApaPunAda.kt
@@ -44,6 +44,7 @@ import com.example.apapunada.ui.LoginScreen
 import com.example.apapunada.ui.components.StaffAppBarPortrait
 import com.example.apapunada.ui.staff.StaffDashboardScreen
 import com.example.apapunada.ui.staff.StaffOrderScreen
+import com.example.apapunada.ui.users.FeedbackNav
 import com.example.apapunada.ui.users.FeedbackScreen
 import com.example.apapunada.ui.users.FeedbackSuccessScreen
 import com.example.apapunada.ui.users.HomeScreen
@@ -97,15 +98,7 @@ fun ApaPunAdaApp(
             }
 
             composable("feedback"){
-                FeedbackScreen(
-                    onSubmit = { navController.navigate("feedbacksuccess")}
-                )
-            }
-
-            composable("feedbacksuccess") {
-                FeedbackSuccessScreen(
-                    onClick = { navController.navigate("home")}
-                )
+                FeedbackNav(navController = navController)
             }
 
             composable("order") {
---
+++ app/src/main/java/com/example/apapunada/ui/users/FeedbackNav.kt
@@ -0,0 +1,48 @@
+package com.example.apapunada.ui.users
+
+import androidx.annotation.StringRes
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.apapunada.R
+import com.example.apapunada.ui.IntroductionPager
+
+enum class FeedbackNav(@StringRes val title: Int){
+    Feedback(R.string.feedback),
+    FeedbackSuccess(R.string.feedback_success),
+    Home(R.string.home)
+}
+
+@Composable
+fun FeedbackNav(
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController = navController, startDestination = FeedbackNav.Feedback.name) {
+        composable(FeedbackNav.Feedback.name){
+            FeedbackScreen(
+                onSubmit = { navController.navigate("feedbacksuccess")}
+            )
+        }
+
+        composable(FeedbackNav.FeedbackSuccess.name) {
+            FeedbackSuccessScreen(
+                onClick = { navController.navigate("home")}
+            )
+        }
+
+        composable(FeedbackNav.Home.name) {
+            HomeScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+@Preview
+fun FeedbackNavPreview(
+
+){
+    FeedbackNav()
+}
